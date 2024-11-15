72dcdbded9d585c33b07e6f2b6a16d98485cbb04
Christian Harjuno <82799882+SorataBaka@users.noreply.github.com>
2024-11-15 10:31:15 +0900

    Add files via upload

---
+++ week-18/src/Slider.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Slider extends JFrame implements ChangeListener {
+    private JSlider slider;
+    private JLabel sliderValue;
+    public Slider(){
+        super();
+        Container container = this.getContentPane();
+        this.slider = new JSlider(0, 100, 50);
+        this.sliderValue = new JLabel(Integer.toString(this.slider.getValue()));
+
+        this.slider.addChangeListener(this);
+
+        JPanel mainFrame = new JPanel();
+        mainFrame.setLayout(new FlowLayout());
+        mainFrame.add(this.slider);
+        mainFrame.add(this.sliderValue);
+
+        container.add(mainFrame);
+    }
+    public void stateChanged(ChangeEvent e){
+        if(e.getSource() == this.slider){
+            this.sliderValue.setText(Integer.toString(this.slider.getValue()));
+        }
+    }
+    public static void main(String[] args){
+        Slider slider = new Slider();
+        slider.setVisible(true);
+        slider.setLocation(0, 0);
+        slider.setExtendedState(Frame.MAXIMIZED_BOTH);
+    }
+}
